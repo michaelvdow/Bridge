27fe0e5d5ed6182306a2bf0841097893e16adae9
szelman <sezelman@gmail.com>
2019-06-01 17:28:05 -0400

    Revert "I'm confused"
    This reverts commit d8779b010a220a17a699cd36373d19ba41cf652f, reversing
    changes made to 6db7d64545cecfc24a0459e87827420ae1018b83.

--- app/src/main/java/com/apps/michaeldow/projectdao/MainActivity/MainActivity.kt
+++ app/src/main/java/com/apps/michaeldow/projectdao/MainActivity/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.apps.michaeldow.projectdao.mainactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.apps.michaeldow.projectdao.R
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.ActionCodeSettings
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
